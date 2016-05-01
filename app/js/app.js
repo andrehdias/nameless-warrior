d64578dd6002dfe04e32c989818878a229790db7
andrehNSFW <andresan2006@gmail.com>
2016-04-30 21:23:29 -0300

    JS components organization

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,3 @@
-// forEach method, could be shipped as part of an Object Literal/Module
-var forEach = function (array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]); // passes back stuff we need
-  }
-};
-
-// closes all popboxes
-var closePopboxes = function(actualPopbox) {
-	var popboxes = document.querySelectorAll('.popbox');
-
-	forEach(popboxes, function(index, popbox) {
-		if(actualPopbox != popbox){
-			popbox.classList.remove('active');					
-		}
-	});
-};
-
 (function() {	
-	var popboxes = document.querySelectorAll('.open-popbox');
-
-	forEach(popboxes, function(index, popbox) {		
-		popbox.addEventListener('click', function(e) {
-			var href = this.dataset.target;			
-
-			closePopboxes(popbox);
-
-			e.preventDefault();
-			document.getElementById(href).classList.toggle('active');		
-		});
-	}); 	
+	new Popbox('.open-popbox');	
 })()
