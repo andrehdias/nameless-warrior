8da16454494ccff9fa86b007b370cfc8ad38115a
andrehNSFW <andresan2006@gmail.com>
2016-05-03 21:53:29 -0300

    page refactoring AGAIN

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,3 @@
 (function() {	
-	var formbox = new Boxes('.open-formbox', '.formbox'),
-			contentBox = new Boxes('.open-contentBox', '.content-box', true, true);
-})()
+	var formbox = new Boxes('.open-formbox', '.formbox');	
+}());
--- app/js/lib/boxes.js
+++ app/js/lib/boxes.js
@@ -1,30 +1,16 @@
 Boxes = function(trigger, target, alwaysOpen, arrow){
 	this.trigger = trigger;
-	this.target = target;
-	this.arrow = (arrow) ? arrow : false;
-	this.alwaysOpen = (alwaysOpen) ? alwaysOpen : false;
+	this.target = target;	
 
 	this.bindEvents();
 };
 
 Boxes.prototype = {
-	// closes all boxes
-	closeBoxes: function(actualBox) {
-		var boxes = document.querySelectorAll(this.target);
-			
-		forEach(boxes, function(index, box) {
-			var href = box.id;
-			
-			if(actualBox != href){
-				box.classList.remove('active');					
-			}
-		});
-	},
-
 	//binds events to elements according the selector passed as parameter
 	bindEvents: function() {
-		var boxes = document.querySelectorAll(this.trigger),
-				_this = this;
+				var _this = this,
+				boxes = document.querySelectorAll(this.trigger),
+				overlay = document.querySelector('.overlay');
 
 		forEach(boxes, function(index, box) {		
 			var href = box.dataset.target,
@@ -35,25 +21,17 @@ Boxes.prototype = {
 			}
 
 			box.addEventListener('click', function(e) {
-				e.preventDefault();
-				_this.closeBoxes(href);				
+				e.preventDefault();				
 
-				if(_this.alwaysOpen) {
-					actualSection.classList.add('active');					
-				} else {
-					actualSection.classList.toggle('active');										
-				}
+				overlay.classList.add('active');
+
+				overlay.addEventListener('click', function() {
+					overlay.classList.remove('active');
+					actualSection.classList.remove('active');
+				});
+				
+				actualSection.classList.add('active');									
 			});
 		}); 		
-	},
-
-	setArrow: function(box, href) {
-		var offset = box.offsetLeft,
-				boxWidth = box.offsetWidth / 2;
-				arrow = document.querySelector("#"+href+" .arrow"),
-				boxOffset = document.querySelector('.content-menu__wrapper').offsetLeft,
-				arrowPosition = offset - boxOffset + boxWidth - 10;
-				console.log(boxWidth)
-		arrow.style.left =  arrowPosition+"px";
 	}
 }
