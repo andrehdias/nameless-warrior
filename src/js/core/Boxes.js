0e17ae39bd050793e8a8badd611f17a1c0f7a242
andreh <andresan2006@gmail.com>
2016-12-13 03:59:25 -0200

    Fix bars css and start new boxes animation

--- src/js/core/Boxes.js
+++ src/js/core/Boxes.js
@@ -5,9 +5,9 @@
 export default class Boxes {
 	constructor(trigger, target) {
 		this.trigger = trigger;
-		this.target = target;		
+		this.target = target;
 
-		this.bindEvents();		
+		this.bindEvents();
 	}
 
 	//binds events to elements according to the selector passed as parameter
@@ -16,38 +16,42 @@ export default class Boxes {
 				boxes = $(this.trigger),
 				overlay = $('.overlay');
 
-		boxes.each(function() {		
+		boxes.each(function() {
 			let href = $(this).data("target"),
 					actualSection = $(href),
 					close = actualSection.find('.formbox__close');
-			
-			close.click((e) => {				
+
+			actualSection.addClass('hide');
+
+			close.click((e) => {
 				e.preventDefault();
 
 				overlay.addClass('hide');
 				actualSection.removeClass('active');
-			});									
+				actualSection.addClass('hide');
+			});
 
-			overlay.click(() => {				
+			overlay.click(() => {
 				overlay.addClass('hide');
 				actualSection.removeClass('active');
-			});									
+				actualSection.addClass('hide');
+			});
 
-			$(this).click((e) => {				
-				e.preventDefault();				
-				
-				overlay.removeClass('hide');
+			$(this).click((e) => {
+				e.preventDefault();
 
 				_this.closeAll();
-				
-				actualSection.addClass('active');									
+
+				overlay.removeClass('hide');
+				actualSection.removeClass('hide');
+				actualSection.addClass('active');
 			});
-		}); 		
+		});
 	}
 
-	closeAll() {		
-		let boxes = $('.formbox');				
-		
+	closeAll() {
+		let boxes = $('.formbox');
+
 		boxes.each(function() {
 			$(this).removeClass('active');
 		});
--- src/js/game/Game.js
+++ src/js/game/Game.js
@@ -2,24 +2,24 @@ import * as states from '../states';
 
 export default class Game {
 	constructor() {
+		const sizes = { w: 980, h: 502 };
+
 		if(!localStorage.getItem('NWarriorToken')) {
 			window.location.assign('/');
-		}		
-
-		const sizes = { w: 980, h: 502 };
+		}
 
 		this.game = new Phaser.Game(sizes.w, sizes.h, Phaser.AUTO, 'phaser');
 
 		Object.keys(states).forEach(state => this.game.state.add(state, states[state]));
 
-		this.game.state.start('Boot');		
+		this.game.state.start('Boot');
 
-		this.uiStyle();		
+		this.uiStyle();
 	}
 
 	uiStyle() {
 		$('.ui-style').each(function() {
 			$(this).append('<div class="ui-style__left-border"></div><div class="ui-style__right-border"></div><div class="ui-style__top-border"></div><div class="ui-style__bottom-border"></div><div class="ui-style__top-left-corner"></div><div class="ui-style__top-right-corner"></div><div class="ui-style__bottom-left-corner"></div><div class="ui-style__bottom-right-corner"></div>');
 		});
-	}	
+	}
 }
